29b86d28644f515db2e6b51d427e00aa1c8d09e2
Viktor <viktor.sbruev.1999@gmail.com>
2025-01-03 19:14:33 +0200

    added cw_8_4

--- CW_8_3.sql
+++ CW_8_3.sql
@@ -2,6 +2,8 @@
 Complete the method that takes a boolean value and returns a string: "Yes" for true or "No" for false. You are given a table named booltoword with a column bool. Write an SQL statement to return a table with two columns: bool and res.
 
 Дополните метод, который принимает булево значение и возвращает строку: "Yes" для true или "No" для false. Вам дана таблица с именем booltoword и столбцом bool. Напишите SQL-запрос, чтобы вернуть таблицу с двумя столбцами: bool и res.
+
+https://www.codewars.com/kata/53369039d7ab3ac506000467
 */
 
 SELECT 
