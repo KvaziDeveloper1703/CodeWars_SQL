2a5732c27f173a63471fe0f9be0987ffceb86a9c
Viktor <viktor.sbruev.1999@gmail.com>
2025-01-03 19:23:00 +0200

    added cw_8_5

---
+++ CW_8_5.sql
@@ -0,0 +1,15 @@
+/*
+Create a function that removes the first and last characters of a string. You are given a table named removechar with a column s. Write an SQL query to return a table with two columns: s and res. You don’t need to handle strings with less than two characters.
+
+Создайте функцию, которая удаляет первый и последний символы строки. Вам дана таблица с именем removechar и столбцом s. Напишите SQL-запрос, чтобы вернуть таблицу с двумя столбцами: s и res. Вам не нужно обрабатывать строки длиной менее двух символов.
+
+https://www.codewars.com/kata/56bc28ad5bdaeb48760009b0
+*/
+
+SELECT 
+    s,
+    SUBSTRING(s, 2, LENGTH(s) - 2) AS res
+FROM 
+    removechar
+WHERE 
+    LENGTH(s) >= 2;
