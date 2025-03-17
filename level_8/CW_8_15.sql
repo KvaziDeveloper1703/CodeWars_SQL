63ef513abf34c7813872e503c92f55040df7c0ce
Viktor <viktor.sbruev.1999@gmail.com>
2025-03-17 16:42:02 +0200

    Added CW_8_15

---
+++ level_8/CW_8_15.sql
@@ -0,0 +1,8 @@
+/*
+Write a function that converts the input string to uppercase.
+
+Напишите функцию, которая преобразует входную строку в верхний регистр.
+*/
+
+SELECT s, UPPER(s) AS res
+FROM makeuppercase;
