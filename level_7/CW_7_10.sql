c660de95ae9244f230bbb3736bbff35d2b44b003
Viktor <viktor.sbruev.1999@gmail.com>
2025-03-17 18:34:36 +0200

    Added CW_7_10

---
+++ level_7/CW_7_10.sql
@@ -0,0 +1,24 @@
+/*
+Треугольные числа называются так, потому что при расположении в виде точек они образуют равносторонний треугольник. 
+
+Например:
+1   2   3
+*   **  ***
+    *   **
+        *
+
+Triangular numbers are named because they form an equilateral triangular shape when laid out as dots. 
+
+For example:
+1   2   3
+*   **  ***
+    *   **
+        *
+*/
+
+SELECT n, 
+       CASE 
+           WHEN n > 0 THEN (n * (n + 1)) / 2
+           ELSE 0
+       END AS res
+FROM triangular;
