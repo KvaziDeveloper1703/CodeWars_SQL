23004b8cb7b7fb6f6202288ca329b6daa4fc737e
Viktor <viktor.sbruev.1999@gmail.com>
2025-04-10 15:22:53 +0300

    Modified CW_8_12

--- level_8/CW_8_12.sql
+++ level_8/CW_8_12.sql
@@ -1,14 +1,14 @@
 /*
-Write an SQL query to process a table named love with columns flower1 and flower2, representing the number of petals on two flowers.
+Write a query to process a table named "love" with columns "flower1" and "flower2", representing the number of petals on two flowers.
 
 The query should return:
-+ Columns flower1 and flower2 from the input table.
-+ A new column named res, which contains true if one flower has an even number of petals and the other has an odd number, and false otherwise.
++ Columns "flower1" and "flower2" from the input table;
++ A new column named "res", which contains true if one flower has an even number of petals and the other has an odd number, and false otherwise.
 
-Напишите SQL-запрос для обработки таблицы love с колонками flower1 и flower2, представляющими количество лепестков на двух цветах.
+Напишите запрос для обработки таблицы "love" с колонками "flower1" и "flower2", представляющими количество лепестков на двух цветах.
 
 Запрос должен вернуть:
-+ Колонки flower1 и flower2 из входной таблицы.
++ Колонки "flower1" и "flower2" из входной таблицы;
 + Новую колонку с именем res, которая содержит true, если у одного цветка чётное количество лепестков, а у другого нечётное, и false в противном случае.
 
 https://www.codewars.com/kata/555086d53eac039a2a000083
