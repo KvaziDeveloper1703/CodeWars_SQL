e236f4c15ddbc5bacb10ad1d86def7068b27218f
Viktor <viktor.sbruev.1999@gmail.com>
2025-04-10 14:52:10 +0300

    Modified CW_8_4

--- level_8/CW_8_4.sql
+++ level_8/CW_8_4.sql
@@ -1,17 +1,7 @@
 /*
-Write a query to find the opposite of a given number. You are provided with a table named opposite, which contains a column number. Return a table with a single column res, where each value is the opposite of the corresponding number.
+Write a query to find the opposite of a given number. You are provided with a table named "opposite", which contains a column "number". Return a table with a single column "res", where each value is the opposite of the corresponding number.
 
-Examples:
-1 → -1
-14 → -14
--34 → 34
-
-Напишите запрос, чтобы найти противоположное переданного числа. Вам предоставлена таблица с именем opposite, которая содержит столбец number. Верните таблицу с одним столбцом res, где каждое значение — это противоположное соответствующему числу из столбца number.
-
-Примеры:
-1 → -1
-14 → -14
--34 → 34
+Напишите запрос, чтобы найти противоположное переданного числа. Вам предоставлена таблица с именем "opposite", которая содержит столбец "number". Верните таблицу с одним столбцом "res", где каждое значение — это противоположное соответствующему числу из столбца "number".
 
 https://www.codewars.com/kata/56dec885c54a926dcd001095
 */
