668b03d4a2c23c18c4f801bf4af1cf891c6e0a22
Viktor <viktor.sbruev.1999@gmail.com>
2025-04-10 21:58:14 +0300

    Modified CW_7_3

--- level_7/CW_7_3.sql
+++ level_7/CW_7_3.sql
@@ -1,17 +1,11 @@
 /*
-Write an SQL query for a table dnastrand with a column dna. Return the original column dna and a new column res containing the complementary DNA strand:
+Write a query for a table "dnastrand" with a column "dna". Return the original column "dna" and a new column "res" containing the complementary DNA strand:
 + "A" ↔ "T"
 + "C" ↔ "G"
 
-Example:
-If dna is "ATCG", res should be "TAGC".
-
-Напишите SQL-запрос для таблицы dnastrand с колонкой dna. Верните колонку dna и новую колонку res с комплементарной цепью ДНК:
+Напишите запрос для таблицы "dnastrand" с колонкой "dna". Верните колонку "dna" и новую колонку "res" с комплементарной цепью ДНК:
 + "A" ↔ "T"
 + "C" ↔ "G"
-
-Пример:
-Если dna равно "ATCG", то res должно быть "TAGC".
 
 https://www.codewars.com/kata/554e4a2f232cdd87d9000038
 */
@@ -21,8 +15,8 @@ SELECT
     UPPER(REPLACE(
             REPLACE(
               REPLACE(
-                REPLACE(dna, 'A', 't'), 
-              'T', 'a'), 
-            'C', 'g'), 
-          'G', 'c')) AS res
+                REPLACE(dna, 'A', 'T'), 
+              'T', 'A'), 
+            'C', 'G'), 
+          'G', 'C')) AS res
 FROM dnastrand;
