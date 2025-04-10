db02efd5d2e8613fc1360baab2f574dd7d31a9f8
Viktor <viktor.sbruev.1999@gmail.com>
2025-04-10 14:58:36 +0300

    Modified CW_8_6

--- level_8/CW_8_6.sql
+++ level_8/CW_8_6.sql
@@ -1,15 +1,15 @@
 /*
-Write an SQL query to process a table named repeatstr with the following columns:
-+ n: the number of repetitions (integer).
-+ s: the string to be repeated (string).
+Write a query to process a table named "repeatstr" with the following columns:
++ n - the number of repetitions;
++ s - the string to be repeated.
 
-Your query should return all the columns from the table and add a new column named res, which contains the string s repeated exactly n times.
+Your query should return all the columns from the table and add a new column named "res", which contains the string s repeated exactly n times.
 
-Напишите SQL-запрос для обработки таблицы repeatstr с колонками:
-+ n: количество повторений (целое число).
-+ s: строка для повторения (строка).
+Напишите запрос для обработки таблицы "repeatstr" с колонками:
++ n - количество повторений;
++ s - строка для повторения.
 
-Запрос должен вернуть все столбцы из таблицы и добавить новый столбец с именем res, содержащий строку s, повторённую ровно n раз.
+Запрос должен вернуть все столбцы из таблицы и добавить новый столбец с именем "res", содержащий строку s, повторённую ровно n раз.
 
 https://www.codewars.com/kata/57a0e5c372292dd76d000d7e
 */
