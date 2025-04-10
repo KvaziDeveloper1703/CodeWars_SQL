1ce5103b2c830e5a8323a2f034f52dd6cd55538e
Viktor <viktor.sbruev.1999@gmail.com>
2025-04-11 02:07:31 +0300

    Modified CW_7_7

--- level_7/CW_7_7.sql
+++ level_7/CW_7_7.sql
@@ -1,9 +1,11 @@
 /*
-Find the total sum of internal angles (in degrees) in an n-sided simple polygon.
+Find the total sum of internal angles in an n-sided simple polygon.
 It is guaranteed that N will be greater than 2.
 
 Найдите сумму всех внутренних углов (в градусах) n-угольного простого многоугольника.
 Гарантируется, что n > 2.
+
+https://www.codewars.com/kata/5a03b3f6a1c9040084001765
 */
 
 SELECT (n - 2) * 180 AS res
