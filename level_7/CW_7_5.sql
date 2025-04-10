c4634100efb509608251f8235bf81113008e4b6f
Viktor <viktor.sbruev.1999@gmail.com>
2025-04-10 22:11:57 +0300

    Modified CW_7_5

--- level_7/CW_7_5.sql
+++ level_7/CW_7_5.sql
@@ -1,13 +1,15 @@
 /*
 Given a divisor and a bound, find the largest integer N such that:
-+ N is divisible by divisor
-+ N is less than or equal to bound
-+ N is greater than 0
++ N is divisible by divisor;
++ N is less than or equal to bound;
++ N is greater than 0.
 
-Даны делитель (divisor) и граница (bound). Найдите наибольшее целое число N, которое удовлетворяет следующим условиям:
-+ N делится на divisor без остатка
-+ N меньше или равно bound
-+ N больше 0
+Даны делитель и граница. Найдите наибольшее целое число N, которое удовлетворяет следующим условиям:
++ N делится на делитель без остатка;
++ N меньше или равно границе;
++ N больше 0.
+
+https://www.codewars.com/kata/5aba780a6a176b029800041c
 */
 
 SELECT (bound / divisor) * divisor AS res
