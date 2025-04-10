08c74c475e5bd16d6ba6411f02266eb09fd38906
Viktor <viktor.sbruev.1999@gmail.com>
2025-04-10 15:13:15 +0300

    Modified CW_8_9

--- level_8/CW_8_9.sql
+++ level_8/CW_8_9.sql
@@ -1,22 +1,12 @@
 /*
-Write an SQL query to process a table named years with a column yr representing a year. The query should return a new column named century, which contains the century the year belongs to.
-
-Century Definition:
-+ The 1st century spans from year 1 to 100.
-+ The 2nd century spans from year 101 to 200.
-+ This pattern continues, where a century is determined by dividing the year by 100 and rounding up to the nearest integer.
+Write a query to process a table named "years" with a column "yr" representing a year. The query should return a new column named "century", which contains the century the year belongs to.
 
 Examples:
 1705 → 18
 1900 → 19
 1601 → 17
 
-Напишите SQL-запрос для обработки таблицы years с колонкой yr, представляющей год. Запрос должен вернуть новую колонку с именем century, содержащую век, к которому относится год.
-
-Определение века:
-+ 1-й век охватывает годы с 1 по 100.
-+ 2-й век охватывает годы с 101 по 200.
-+ Этот принцип продолжается: век определяется делением года на 100 с округлением вверх до ближайшего целого числа.
+Напишите запрос для обработки таблицы "years" с колонкой "yr", представляющей год. Запрос должен вернуть новую колонку с именем "century", содержащую век, к которому относится год.
 
 Примеры:
 1705 → 18
