bd0fe16f6b3853c022ec47abb3a2979ee24d2c43
Viktor <viktor.sbruev.1999@gmail.com>
2025-04-10 14:38:38 +0300

    Modified CW_8_1

--- level_8/CW_8_1.sql
+++ level_8/CW_8_1.sql
@@ -1,6 +1,6 @@
 /*
-You will be given a table numbers with one column number.
-You need to return a dataset with two columns: number and is_even. The number column should contain the original values, and the is_even column should contain the strings "Even" (for even numbers) or "Odd" (for odd numbers) based on the values in the number column.
+You will be given a table "numbers" with one column "number".
+You need to return a dataset with two columns: "number" and "is_even". The "number" column should contain the original values, and the "is_even" column should contain the strings "Even" or "Odd" based on the values in the "number" column.
 
 Input Table Schema:
 number INT
@@ -9,8 +9,8 @@ Output Table Schema:
 number INT
 is_even STRING
 
-Вам будет предоставлена таблица numbers с одним столбцом number.
-Необходимо вернуть набор данных с двумя столбцами: number и is_even. В столбце number должны быть исходные значения, а в столбце is_even — строки "Even" (четное) или "Odd" (нечетное) в зависимости от значения столбца number.
+Вам будет предоставлена таблица "numbers" с одним столбцом "number".
+Необходимо вернуть набор данных с двумя столбцами: "number" и "is_even". В столбце "number" должны быть исходные значения, а в столбце "is_even" — строки "Even" или "Odd" в зависимости от значения столбца "number".
 
 Схема таблицы входных данных:
 number INT
