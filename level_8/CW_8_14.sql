95f3e82aeadf96543e24534a859be48e53b12848
Viktor <viktor.sbruev.1999@gmail.com>
2025-04-10 15:37:24 +0300

    Modified CW_8_14

--- level_8/CW_8_14.sql
+++ level_8/CW_8_14.sql
@@ -1,37 +1,29 @@
 /*
-Write an SQL query to calculate the total number of blank pages required for your classmates' paperwork. The table paperwork contains the following columns:
-+ id: Unique identifier for each record.
-+ n: Number of classmates.
+Write a query to calculate the total number of blank pages required for your classmates paperwork. The table "paperwork" contains the following columns:
++ id: Unique identifier for each record;
++ n: Number of classmates;
 + m: Number of pages in the paperwork.
 
 The result should be a new table with columns:
-+ n: Number of classmates.
-+ m: Number of pages.
-+ res: The total number of blank pages required (calculated as n * m).
-
-Rules:
-+ If either n or m is less than 0, the result should be 0.
++ n - Number of classmates;
++ m - Number of pages;
++ res - The total number of blank pages required.
 
 Examples:
 n = 5, m = 5 --> res = 25
-n = -5, m = 5 --> res = 0
 
-Напишите SQL-запрос для вычисления общего количества чистых страниц, необходимых для выполнения работы для одноклассников. Таблица paperwork содержит следующие столбцы:
-+ id: Уникальный идентификатор для каждой записи.
-+ n: Количество одноклассников.
-+ m: Количество страниц в работе.
+Напишите запрос для вычисления общего количества чистых страниц, необходимых для выполнения работы для одноклассников. Таблица "paperwork" содержит следующие столбцы:
++ id - Уникальный идентификатор для каждой записи;
++ n - Количество одноклассников;
++ m - Количество страниц в работе.
 
 Результат должен быть представлен в новой таблице со столбцами:
-+ n: Количество одноклассников.
-+ m: Количество страниц.
-+ res: Общее количество чистых страниц (вычисляется как n * m).
++ n - Количество одноклассников;
++ m - Количество страниц;
++ res - Общее количество чистых страниц.
 
-Правила:
-+ Если n или m меньше 0, результат должен быть 0.
-
-Примеры:
+Пример:
 n = 5, m = 5 --> res = 25
-n = -5, m = 5 --> res = 0
 
 https://www.codewars.com/kata/55f9b48403f6b87a7c0000bd
 */
