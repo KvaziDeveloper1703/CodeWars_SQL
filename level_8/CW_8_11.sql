8840a7b68edd69fd40e7bb7b301eff1122433752
Viktor <viktor.sbruev.1999@gmail.com>
2025-04-10 15:20:08 +0300

    Modified CW_8_11

--- level_8/CW_8_11.sql
+++ level_8/CW_8_11.sql
@@ -1,12 +1,10 @@
 /*
-Write an SQL query to process a table named person with a column name. The query should return a new column named greeting, which contains the following string for each name:
+Write a query to process a table named "person" with a column "name". The query should return a new column named "greeting", which contains the following string for each name:
 "Hello, <name> how are you doing today?"
-
 Make sure the output matches the exact format specified.
 
-Напишите SQL-запрос для обработки таблицы person с колонкой name. Запрос должен вернуть новую колонку с именем greeting, которая содержит следующую строку для каждого name:
+Напишите запрос для обработки таблицы "person" с колонкой "name". Запрос должен вернуть новую колонку с именем "greeting", которая содержит следующую строку для каждого name:
 "Hello, <name> how are you doing today?"
-
 Убедитесь, что вывод точно соответствует указанному формату.
 
 https://www.codewars.com/kata/55a70521798b14d4750000a4
