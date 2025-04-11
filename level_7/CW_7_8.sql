ce724833bec877061817f69787c238299dbadec3
Viktor <viktor.sbruev.1999@gmail.com>
2025-04-11 12:36:03 +0300

    Modified CW_7_8

--- level_7/CW_7_8.sql
+++ level_7/CW_7_8.sql
@@ -2,9 +2,11 @@
 Write a query to list the top 5 bestselling books (name, author, copies sold) from the "books" table.
 
 Напишите запрос, который выведет 5 самых продаваемых книг (название, автор, количество проданных копий) из таблицы "books".
+
+https://www.codewars.com/kata/591127cbe8b9fb05bd00004b
 */
 
 SELECT name, author, copies_sold
 FROM books
 ORDER BY copies_sold DESC
-LIMIT 5;
+LIMIT 5; 
